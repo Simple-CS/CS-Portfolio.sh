e0f49ed2e49a1917dc3adf4085fd82e2a92ec53a
simple_cs <eedelhart.sh@mysummitps.org>
2016-01-11 20:53:06 +0000

    Last commit

--- New Folder/info.js
+++ New Folder/info.js
@@ -4,22 +4,27 @@ var item3 = document.getElementById("item3");
 var item4 = document.getElementById("item4");
 var info1 = document.getElementById("info1");
 var info2 = document.getElementById("info2");
-
+var info3 = document.getElementById("info3");
+var info4 = document.getElementById("info4");
+var info5 = document.getElementById("info5");
+var info6 = document.getElementById("info6");
+var info7 = document.getElementById("info7");
+var info8 = document.getElementById("info8");
 item1.addEventListener("click", function(){
-    info1.innerHTML = "The DaVinci Code, written by author Dan Brown, is about art historian and symbologist <br> Robert Langdon and cryptologist Sophie Neveu investigating a murder at the Lourve in Paris,France. <br>Langdon and Neveu spend the book connecting the murder to various groups that have little known <br>about them but a large connection to history. The murder leads Langdon and Neveu down many paths, <br>with danger lurking around every corner, since their dealings left them in contact with shady <br>organizations that little is known about.  ";
+    info1.innerHTML = "The DaVinci Code, written by author Dan Brown, is about art historian and symbologist <br> Robert Langdon and cryptologist Sophie Neveu investigating a murder at the Lourve in Paris,France. <br>Langdon and Neveu spend the book connecting the murder to various groups that have little known <br>about them but a large connection to history. The murder leads Langdon and Neveu down many paths, <br>with danger lurking around every corner, since their dealings left them in contact with shady <br> organizations that little is known about.  ";
     info2.innerHTML = "<br>This book is in fact one of my favourite of all time because of the mystery and intrigue of the story. At no <br> point when reading this book did I feel compelled to put it down due to immense boredom.<br> Dan Brown has remained one of my all-time favourite authors with his release of 'Inferno' <br>in May of 2013.";
 });
                        
 item2.addEventListener("click", function(){
-    info1.innerHTML = "Have you ever wanted to know the exact mathematical odds of winning the Powerball?<br> Or, maybe you're not a gambler, but a religion expert trying to figure out how to interpret <br> the existence of God using algorithms while examining religious texts. If these situations <br> seem as if they may apply to you, then I would absolutely suggest 'How to Not Be Wrong' <br> by Jordan Ellenberg. Ellenberg, a mathematician and professor at University of Wisconsin-Madison, <br> talks all about the mathematical situations we encounter on a daily basis,from out misunderstanding <br> to things we haven't thought about. Ellenberg explains concepts in a way that is both interesting and informational.";
-    info2.innerHTML = "<br>";
+    info3.innerHTML = "Have you ever wanted to know the exact mathematical odds of winning the Powerball?<br> Or, maybe you're not a gambler, but a religion expert trying to figure out how to interpret <br> the existence of God using algorithms while examining religious texts. If these situations <br> seem as if they may apply to you, then I would absolutely suggest 'How to Not Be Wrong' <br> by Jordan Ellenberg. Ellenberg, a mathematician and professor at University of Wisconsin-Madison, <br> talks all about the mathematical situations we encounter on a daily basis,from out misunderstanding <br> to things we haven't thought about. Ellenberg explains concepts in a way that is both interesting<br> and informational.";
+    info4.innerHTML = "<br> This is a book that I would recommend to any serious lover of math, as well as anyone <br> who wants to learn math in a non-boring, entrhalling way. It was a read that not only <br> taught me more about math, but it was one that kep me engaged. For any of you out there looking <br> to spend a week or test learning tid-bits of information about real-world applications <br> of mathematical concepts, this is the book for you. ";
 });
 item3.addEventListener("click",function(){
-    info1.innerHTML ="Item 3 stuff";
-    info2.innerHTML ="Item 3 other stuff";
+    info5.innerHTML ="Out of all four of the titles that I am presenting to you today, 'Ham on Rye' written by <br> Charles Bukowski is by far my favourite. I really do enjoy the sentiment of this abrasive and <br> truthful coming-of-age tale, written in the perspective of Bukowski's long time alter ego Henry <br> Chinaski. The no-holds-barred approach to writing left me constantly entertained and waiting <br> to see what story he would have around the next corner. This book goes from stories about <br> Chinaski's dealings with his abusive and generally awful father, to his exploratory trips with <br> friends and classmates throughout LA. ";
+    info6.innerHTML ="<br> This book is one that deserves a recommendation to just about any reader. If you enjoy <br> good story telling, humour, a bit of sadness, or just about anything else short of fantasy in <br> your books, then 'Ham on Rye' is for you. It is hard to find much wrong with the book, although <br> I must make it known that the writing is slightly coarse and crude, but wonderful nonetheless.";
 });    
 item4.addEventListener("click",function(){
-    info1.innerHTML = "Item 4 stuff";
-    info2.innerHTML = "Item 4 other stuff";
+    info7.innerHTML = "Finally, I have the fourth and conclusive book to present to you all today,and that is <br> 'Gone Girl' written by Gillian Flynn. This story, about a missing woman and the adventure <br> (if I can even call it an adventure) to find her is heart-pounding, mysterious, and absolutely exquisite.<br> There are many twists and turns throughout this 420 page book, making it so you never know <br> what to expect on the next page.  ";
+    
 })
 // JavaScript File
